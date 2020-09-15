ce598d0b7f3955baab006fd0edac1d9be2aa31d1
gbrandt555 <georgebrandt7841@gmail.com>
2020-09-15 16:56:07 -0500

    new commit

--- store-inventory/app.py
+++ store-inventory/app.py
@@ -1,4 +1,5 @@
 from peewee import *
+from collections import OrderedDict
 
 import csv
 import datetime
@@ -6,7 +7,7 @@ import os
 
 
 db = SqliteDatabase("inventory.db")
-
+new_inventory = []
 
 class Product(Model):
     product_id = AutoField()
@@ -15,15 +16,78 @@ class Product(Model):
     product_price = IntegerField(default=0)
     date_updated = DateTimeField()
 
+    class Meta:
+        database = db
 
-with open("inventory.csv", newline='') as csvfile:
-    product_reader = csv.DictReader(csvfile, delimiter=',')
-    rows = list(product_reader)
-    for row in rows:
-        print(row)
+
+def clean_data():
+    with open("inventory.csv", newline='') as csvfile:
+        product_reader = csv.DictReader(csvfile, delimiter=',')
+        rows = list(product_reader)
+        for row in rows:
+            row['product_quantity'] = int(row['product_quantity'])
+            row['product_price'] = int(row['product_price'].replace('$', '').replace('.', ''))
+            row['date_updated'] = datetime.datetime.strptime(row['date_updated'], '%m/%d/%Y')
+
+            new_inventory.append(dict(
+                product_name = row['product_name'],
+                product_quantity = row['product_quantity'],
+                product_price = row['product_price'],
+                date_updated = row['date_updated']
+            ))
+        
+
+
+def menu_loop():
+    choice = None
+    choices = ['v', 'a', 'b']
+    while choice != 'q':
+        print("Store Inventory...\n\n")
+        print("Choose one of the following options or press 'q' to quit.\n")
+        for key, value in menu.items():
+            print("{}) {}".format(key, value.__doc__)) 
+        choice = input("\nChoose an option: ").lower().strip()
+        if choice not in choices:
+            print("\n\nInvalid option, please try again")
+        elif choice in menu:
+            menu[choice]()
+
+def view_entry():
+    """View a single product's inventory"""
+    new_id = input("Enter the product ID: ")
+    entries = Product.select().where(Product.product_id == new_id)
+    if entries:
+        print("Product ID: {}\n".format(new_id))
+        for entry in entries:
+            print("Product name: {}".format(entry.product_name))
+            print("Product price: {}".format(entry.product_price))
+            print("Product quantity: {}".format(entry.product_quantity))
+            print("Date updated: {}".format(entry.date_updated))
+
+
+def add_entry():
+    """Add a new product to the database"""
+
+
+def backup_data():
+    """Make a backup of the entire inventory"""
+
+
+
+menu = OrderedDict([
+    ('v', view_entry),
+    ('a', add_entry),
+    ('b', backup_data),
+])    
+    
+       
+        
+
 
 
 
 if __name__ == "__main__":
     db.connect()
     db.create_tables([Product], safe=True)
+    clean_data()
+    menu_loop()
